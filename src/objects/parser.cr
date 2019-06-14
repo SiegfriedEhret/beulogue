a54f2b7461191cda76aa3b1d8729f66772c3aca4
Siegfried Ehret <siegfried@ehret.me>
2019-06-14 21:44:48 +0200

    :gift: update code

--- src/objects/parser.cr
+++ src/objects/parser.cr
@@ -4,19 +4,29 @@ class BeulogueParserFS < Crustache::HashFileSystem
   def load(name)
     if /\Adailymotion\s+([\w\-_]+)\z/ === name
       id = $1
+
       html = <<-HTML
       <iframe
         width="560" height="315"
         src="https://www.dailymotion.com/embed/video/#{id}"
         frameborder="0"
         allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen ><`/iframe>
+        allowfullscreen ></iframe>
+      HTML
+    end
 
+    if /\Agist\s+([\w\-_]+)\s+([\w\-_]+)\z/ === name
+      user = $1
+      id = $2
+
+      html = <<-HTML
+      <script src="https://gist.github.com/#{user}/#{id}.js"></script>
       HTML
     end
 
     if /\Avimeo\s+([\w\-_]+)\z/ === name
       id = $1
+
       html = <<-HTML
       <iframe src="https://player.vimeo.com/video/#{id}"
         width="640" height="360"
@@ -28,6 +38,7 @@ class BeulogueParserFS < Crustache::HashFileSystem
 
     if /\Ayoutube\s+([\w\-_]+)\z/ === name
       id = $1
+
       html = <<-HTML
       <iframe
         width="560" height="315"
@@ -36,7 +47,9 @@ class BeulogueParserFS < Crustache::HashFileSystem
         allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
         allowfullscreen></iframe>
       HTML
+    end
 
+    if html
       return Crustache.parse html
     end
 
